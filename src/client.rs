d1f6b07100e9a21812629f712ac06f8f570fcbb7
Algorithm5838 <108630393+Algorithm5838@users.noreply.github.com>
2026-04-30 11:41:54 +0300

    perf(client): increase json cache TTL to 120s and size to 1000

--- src/client.rs
+++ src/client.rs
@@ -354,7 +354,7 @@ fn request(method: &'static Method, path: String, redirect: bool, quarantine: bo
 }
 
 /// Make a request to a Reddit API and parse the JSON response
-#[cached(size = 500, time = 30, result = true)]
+#[cached(size = 1000, time = 120, result = true)]
 pub async fn json(path: String, quarantine: bool) -> Result<Value, String> {
 	// Closure to quickly build errors
 	let err = |msg: &str, e: String, path: String| -> Result<Value, String> {
